ce743ff949edbda7b9b7bfbb640f265effee4190
Bander Btati <bander.mohammed.2000@gmail.com>
2026-03-14 04:59:25 +0300

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
-import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
-import RootLayout from './pages/Root';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from "./pages/Home";
+// import PostPage, { loader as postLoader } from "./pages/Post";
+import RootLayout from "./pages/Root";
+import { lazy, Suspense } from "react";
+
+// Lazy Loading
+const BlogPage = lazy(() => import("./pages/Blog"));
+const PostPage = lazy(() => import("./pages/Post"));
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       {
@@ -15,10 +20,32 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
       {
-        path: 'posts',
+        path: "posts",
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              // Lazy Loading
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            // Lazy Loading
+            loader: () =>
+              import("./pages/Blog").then((module) => module.loader()),
+          },
+          {
+            path: ":id",
+            // Lazy Loading
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            // Lazy Loading
+            loader: (meta) =>
+              import("./pages/Post").then((module) => module.loader(meta)),
+          },
         ],
       },
     ],
